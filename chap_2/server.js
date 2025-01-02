b6337f210a4faa626284e13e9f8139e9edbe52b8
AbangAyoma <abangayoma@gmail.com>
2025-01-02 23:03:11 +0100

    chap2

---
+++ chap_2/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 3434;
+
+
+
+app.listen(PORT, () => {
+    console.log('\n...............App is listeningn.....................\n')
+})
